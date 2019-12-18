69623d15f424bdb5f69452db9feaf1af1479f4af
Chaejin Lim <50129809+chaejin98@users.noreply.github.com>
2019-12-18 23:02:29 +0900

    Writing CSV Reader

--- Detection/src/Main.java
+++ Detection/src/Main.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 
 
@@ -14,13 +17,62 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String csvFile="../imageoutput.csv";
-		BufferedReader br=null;
-		String lime ="";
+		String csvoutput="output_modify.csv";
+		BufferedReader br_init=null;
+		String line ="";
 		String cvsSplitBy=",";
+		List<List<String>> set=new ArrayList<List<String>>();
+		
+		try {
+			br_init=Files.newBufferedReader(Paths.get(csvoutput));
+			Charset.forName("UTF-8");
+	
+			while((line=br_init.readLine())!=null) {
+				List<String> temp=new ArrayList<String>();
+				String array[]=line.split(cvsSplitBy);
+				temp=Arrays.asList(array);
+				System.out.println(temp.get(0));
+				set.add(temp);
+			}
+			String array[]=line.split(cvsSplitBy);
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(br_init!=null) {
+					br_init.close();
+				}
+			}catch(IOException e) {
+				e.printStackTrace();
+			}	
+		}
+		
+		
+		
+		String csvFile="imageoutput.csv";
+		BufferedReader br=null;
+		String image[];
 		try {
 			br=Files.newBufferedReader(Paths.get(csvFile));
 			Charset.forName("UTF-8");
+			line=br.readLine();
+			image=line.split(cvsSplitBy);
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(br!=null) {
+					br.close();
+				}
+			}catch(IOException e) {
+				e.printStackTrace();
+			}	
+		}
+		
+		
 	}
-
 }
