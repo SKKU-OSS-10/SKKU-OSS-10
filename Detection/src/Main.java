e05732b21b1492dccfdfcd9d133a74f371b202a6
Chaejin Lim <50129809+chaejin98@users.noreply.github.com>
2019-12-18 22:42:07 +0900

    Write cvs reader

--- Detection/src/Main.java
+++ Detection/src/Main.java
@@ -1,9 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.charset.Charset;
+
+
+
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String csvFile="../imageoutput.csv";
+		BufferedReader br=null;
+		String lime ="";
+		String cvsSplitBy=",";
+		try {
+			br=Files.newBufferedReader(Paths.get(csvFile));
+			Charset.forName("UTF-8");
 	}
 
 }
